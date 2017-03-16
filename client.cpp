56cf400110249693b39b92e7996902c4b0462d2f
OPzCatchMee <cmnguyen999@gmail.com>
2017-03-15 19:52:07 -0500

    first commit

---
+++ client.cpp
@@ -0,0 +1,38 @@
+// Chien Nguyen
+// PS# 1328764
+
+// CLIENT
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  string hostName = "";
+  int portNumber = 0;
+
+  // Prompt uesr for user name
+  cout << "Enter a server host name: ";
+  cin >> hostName;
+
+  // Prompt user for server's port number
+  cout << "Enter server port number: ";
+  cin >> portNumber;
+
+  // Create a socket
+
+  // Connect it to server
+
+  // Send the user name to the server
+
+  // Wait for the user's private key
+
+  // Close the socket
+
+  // Print out the key it got from the server
+
+
+  return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,36 @@
+// Chien Nguyen
+// PS# 1328764
+
+// SERVER
+
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+
+  // Create a socket
+
+  // Bind an address to that socket
+
+  // Set up the buffer size for the socket
+
+  // Wait for incoming calls
+
+  // Accept incoming calls (and get a new socket)
+
+  // Reply with the requested public key
+
+  // Hang up
+
+  // Repeat steps 4 and 7
+
+
+  return 0;
+}
