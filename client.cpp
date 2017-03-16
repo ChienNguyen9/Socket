1cbbb27c48f955010df13a0fe9a21c8aa6dbc94a
OPzCatchMee <cmnguyen999@gmail.com>
2017-03-15 21:39:01 -0500

    Create a socket for the client

--- client.cpp
+++ client.cpp
@@ -5,13 +5,23 @@
 
 #include <iostream>
 #include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
 
 using namespace std;
 
 int main() {
 
-  string hostName = "";
-  int portNumber = 0;
+  string hostName;
+  int portNumber, sock;
+  const int bufferSize = 1024;
+  char buffer[bufferSize];
+
+  struct sockaddr_in sa;
 
   // Prompt uesr for user name
   cout << "Enter a server host name: ";
@@ -22,14 +32,32 @@ int main() {
   cin >> portNumber;
 
   // Create a socket
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if(sock < 0) {
+    cout << "Could not create socket..." << endl;
+    return;
+  }
+
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(portNumber);
+  if (bind(s, &sa, sizeof(struct sockaddr_in)) < 0) {
+    cout << "Could not bind socket..." << endl;
+    return;
+  }
 
   // Connect it to server
+  if(connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+    cout << "Could not connect to server..." << endl;;
+    return;
+  }
 
   // Send the user name to the server
+
 
   // Wait for the user's private key
 
   // Close the socket
+  close(sock);
 
   // Print out the key it got from the server
 
