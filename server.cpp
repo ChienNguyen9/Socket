496195aa1791756b8e495291790e958bc8382a70
OPzCatchMee <cmnguyen999@gmail.com>
2017-03-17 00:21:57 -0500

    Added code to get all the info from file

--- server.cpp
+++ server.cpp
@@ -4,12 +4,14 @@
 // SERVER
 
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -21,16 +23,30 @@ struct serverTable{
 int main() {
 
   serverTable table[1024];
-  string fileName, terminate = "something";
+  int count = 0;
+  string fileName, tempID, tempKey, terminate = "something";
+  bool running = true;
   int sock, portNumber, server;
-  const int bufferSize = 1024;
-  char buffer[bufferSize];
+  int bufferSize = 1024;
+  string chat[bufferSize];
 
   struct sockaddr_in sa;
+
+  memset(&sa, 0, sizeof(struct sockaddr_in));
 
   // Prompt user for file name
   cout << "Enter a file name: ";
   cin >> fileName;
+
+  // Gets all the info from the file and puts it in table
+  ifstream file;
+  file.open(fileName);
+  while(fileName >> tempID >> tempKey) {
+    table[count].userID = tempID;
+    table[count].publicKey = tempKey;
+    count++;
+  }
+  fileName.close();
 
   // Prompt user for server's port number
   cout << "Enter server port number: ";
@@ -40,43 +56,39 @@ int main() {
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if(sock < 0) {
     cout << "Could not create socket..." << endl;
-    return;
+    return (-1);
   }
 
   // Bind an address to that socket
   sa.sin_family = AF_INET;
   sa.sin_port = htons(portNumber);
-  if (bind(s, &sa, sizeof(struct sockaddr_in)) < 0) {
+  if (bind(sock, &sa, sizeof(struct sockaddr_in)) < 0) {
     cout << "Could not bind socket..." << endl;
-    return;
+    return (-1);
   }
 
   // Set up the buffer size for the socket
-  size = sizeof(server_addr);
+  size = sizeof(sa);
   listen(sock, 3);
 
   // Wait for incoming calls - Accept()
-  server = accept(sock, (struct sockaddr*)&server_addr, &size);
+  server = accept(sock, (struct sockaddr*)&sa, &size);
   if(server < 0) {
     cout << "Could not accept server..." << endl;
-    return;
+    return (-1);
   }
 
   cout << "Type \"Terminate.\" to exit. \t" << endl;
 
   // Accept incoming calls (and get a new socket) - client says
-  /* while((terminate != "Terminate.") || (terminate != "Terminate") || (terminate != "terminate")) {
-    send(server, buffer, bufferSize, 0);
+  while((terminate != "Terminate.") || (terminate != "Terminate") || (terminate != "terminate")) {
+    send(server, chat, bufferSize, 0);
   }
-  */
 
   // Reply with the requested public key
 
   // Hang up
   close(sock);
-
-  // Repeat steps 4 and 7
-
 
   return 0;
 }
